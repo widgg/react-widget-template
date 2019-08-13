227e056e9dbd46e1348be0b2351fe204f3667e51
Ariel Cabib <ariel.haviv@gmail.com>
2019-08-13 15:43:27 +0300

    webpack mode production

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 const glob = require("glob")
 
 module.exports = {
+  mode: 'production',
   entry: {
     "app.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
   },
